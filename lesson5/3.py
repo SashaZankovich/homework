76daf66b8d6b0e0da67a8abd18c8ca2db2e1a8ad
SashaZankovich <zankovich_sasha@mail.ru>
2025-02-04 18:35:27 +0300

    5

--- lesson5/3.py
+++ lesson5/3.py
@@ -8,13 +8,12 @@ d = {'one':11, 'two':22, 'hello':'python', True:False}
 d = {'one':11, 'two':22, 'hello':'python', True:False}
 
 c = list(d.items())
-print(c)
-
-#c = list(d)
 
 user_data = int(input("Введите номер элемента, который необходимо удалить "))
 
 del c[user_data]
+
+print(c)
 
 
 
